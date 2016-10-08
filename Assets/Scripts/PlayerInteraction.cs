04d59e20ee112dd0ba3e2bd5a44a037ad94a6df3
Konrad Feiler <konrad@tactica.de>
2016-10-08 19:27:03 -0400

    add audio

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteraction : MonoBehaviour {
+
+	public GameObject mainCam;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			Debug.Log ("Space pressed");
+			transform.position = transform.position + mainCam.transform.forward;
+		}
+	}
+}

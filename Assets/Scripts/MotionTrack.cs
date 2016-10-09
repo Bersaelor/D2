43bb69798ea3af262bf53ecb5acdbfa02ed034b1
Xiaoyan Shen <xyshen@mit.edu>
2016-10-09 12:41:53 -0400

    explotion assets

---
+++ Assets/Scripts/MotionTrack.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionTrack : MonoBehaviour {
+
+	ArrayList pos = new ArrayList ();
+
+	bool isRec = false;
+	float tempX;
+	float tempY;
+	float tempZ;
+
+	// Use this for initialization
+	void Start () {
+		isRec = true;
+		Debug.Log ("Start Recording!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isRec == true) {
+			tempX = transform.position.x;
+			tempY = transform.position.y;
+			tempZ = transform.position.z;
+			pos.Add (tempX);
+			pos.Add (tempY);
+			pos.Add (tempZ);
+			Debug.Log ("now X track" + pos);
+			Debug.Log ("Current X pos" + tempX);
+
+		}
+	
+	}
+}

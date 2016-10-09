f10b354a47f0f3a12a1d60c9699781127d8bd240
Konrad Feiler <konrad@tactica.de>
2016-10-08 21:36:58 -0400

    clamp position movement, add little crosshair

---
+++ Assets/Plugins/RayScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayScript : MonoBehaviour {
+
+	//private bool canRotate;
+	private GameObject cachedObj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		cachedObj = null;
+
+		RaycastHit hitInfo;
+		if (Physics.Raycast (Camera.main.transform.position, Camera.main.transform.forward, out hitInfo )) {
+			if (hitInfo.collider.gameObject.CompareTag ("Gazable")) {
+				//canRotate = true;
+				Debug.Log("On Gazable");
+				cachedObj = hitInfo.collider.gameObject;
+			}
+		}
+
+		if (cachedObj != null) {
+			cachedObj.transform.Rotate(15.0f * Time.deltaTime, 15.0f * Time.deltaTime, 15.0f * Time.deltaTime);
+		}
+	
+	}
+}
--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -4,17 +4,34 @@ using System.Collections;
 public class PlayerInteraction : MonoBehaviour {
 
 	public GameObject mainCam;
+	public GameObject selectedTerrain;
+	private float initialHeight = 0;
 
 	// Use this for initialization
 	void Start () {
-	
+		initialHeight = transform.position.y - getTerrain().SampleHeight (transform.position);
+		Debug.Log ("Initial height is " + initialHeight);
 	}
-	
+
+	Terrain getTerrain() {
+		return selectedTerrain.GetComponent<Terrain> ();
+		//		return Terrain.activeTerrain;
+	}
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("space")) {
-			Debug.Log ("Space pressed");
-			transform.position = transform.position + mainCam.transform.forward;
+		if (Input.GetButton ("Fire1")) {
+			float maxPos = 110;
+
+			Vector3 newPos = transform.position + mainCam.transform.forward;
+			newPos.y = getTerrain().SampleHeight (transform.position) + initialHeight;
+			newPos.x = Mathf.Min (newPos.x, maxPos);
+			newPos.x = Mathf.Max (newPos.x, -maxPos);
+			newPos.z = Mathf.Min (newPos.z, maxPos);
+			newPos.z = Mathf.Max (newPos.z, -maxPos);
+			Debug.Log ("newPos" + newPos);
+
+			transform.position = newPos;
 		}
 	}
 }
